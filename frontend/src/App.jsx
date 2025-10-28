ce0e778b7c87a935a00f51678d6d6b36b7d4676a
Manohar Poojary <poojarymanohara967@gmail.com>
2025-10-29 00:06:17 +0530

    connected tailwind and axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/v1/user/test")
+      .then(res => {
+        setData(res.data.data);
+        console.log(res.data.data); // ✅ log actual response
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ol>
+      <li className="list-decimal list-inside pl-6 text-amber-600">hello</li>
+    </ol>
+    <ul className="list-disc list-inside text-amber-600">{
+        data.map((item)=>(<li key={item.id}>{item.joke}</li>))
+      }
+    </ul>
+
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
