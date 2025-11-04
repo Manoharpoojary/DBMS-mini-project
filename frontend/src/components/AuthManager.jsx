30e97ae574d9c65286beb19fc27e6dd9017ca747
Manohar Poojary <poojarymanohara967@gmail.com>
2025-11-05 00:25:14 +0530

    add initial component files for AddEmp, AddManager, AuthManager, Bus, BusAssign, Employee, Login, MainPage, and Routes

---
+++ frontend/src/components/AuthManager.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthManager = () => {
+  return (
+    <div>AuthManager</div>
+  )
+}
+
+export default AuthManager
---
+++ frontend/src/components/BusAssign.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BusAssign = () => {
+  return (
+    <div>BusAssign</div>
+  )
+}
+
+export default BusAssign
